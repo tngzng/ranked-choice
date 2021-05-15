bc7900ba9868e8f6a9e13b8497a8e8795f34982f
ting zhang <thetingzhang@gmail.com>
2021-05-15 19:27:30 -0400

    [Add] candidates eliminated by the 5th round to report

--- election.py
+++ election.py
@@ -1,12 +1,19 @@
+import operator
 from typing import List, Tuple
-import numpy as np
 
+import numpy as np
 from pyrankvote import Candidate, Ballot, instant_runoff_voting
 from pyrankvote.helpers import ElectionResults, CandidateStatus
 from progressbar import progressbar
 
+
+SIMULATIONS = 100
+VOTERS = 1000
+# rank up to 5 candidates in order of preference
+# source: https://www1.nyc.gov/site/civicengagement/voting/ranked-choice-voting.page
+VOTES = 5
 # source: https://www.nydailynews.com/news/politics/nyc-elections-2021/ny-nyc-mayoral-race-poll-latest-20210513-o6g7vjptdfgwzelttmin5t6qla-story.html
-candidate_weights = [
+CANDIDATE_WEIGHTS = [
     ("andrew yang", 0.21),
     ("eric adams", 0.17),
     ("scott stringer", 0.1),
@@ -19,60 +26,74 @@ candidate_weights = [
 
 
 def generate_ballot(candidates=List[Candidate], weights=List[float]) -> Ballot:
-    # rank up to 5 candidates in order of preference
-    # source: https://www1.nyc.gov/site/civicengagement/voting/ranked-choice-voting.page
-    votes = 5
-    ballot = np.random.choice(candidates, p=weights, replace=False, size=votes)
+    ballot = np.random.choice(candidates, p=weights, replace=False, size=VOTES)
     return Ballot(ranked_candidates=ballot)
 
 
 def run_election(candidate_weights: List[Tuple[str, float]]) -> ElectionResults:
-    # total votes in 2017 nyc democratic mayoral primary
-    # source: https://en.wikipedia.org/wiki/2017_New_York_City_mayoral_election
-    voters = 437517
-
     candidates = [Candidate(p[0]) for p in candidate_weights]
     weights = np.array([p[1] for p in candidate_weights])
     adjusted_weights = weights / weights.sum()
 
-    ballots = [generate_ballot(candidates, adjusted_weights) for v in range(voters)]
-    return instant_runoff_voting(candidates, ballots)
+    ballots = [generate_ballot(candidates, adjusted_weights) for v in range(VOTERS)]
+    results = instant_runoff_voting(candidates, ballots)
+    return results
 
 
-def get_eliminated(results: ElectionResults, round_num: int) -> List[Candidate]:
-    _round = results.rounds[round_num]
+def get_eliminated(results: ElectionResults) -> List[Candidate]:
+    """
+    get list of candidates eliminated in the round that corresponds with the
+    number of choices each voter gets.
+
+    for example, if voters get to select five candidates, return the candidates who
+    have been eliminated by the fifth round.
+
+    this attempts to identify which ballots may lead to the following scenario:
+    "Say there are five candidates running, but the voter ranks only three, and all three are eliminated prior to the last round. As a result, none of their votes will have gone to the winning candidate or the runner-up. In effect, their ballot doesn’t figure in the outcome."
+
+    source: https://democracyjournal.org/arguments/ranked-choice-voting-is-not-the-solution/
+    """
+    try:
+        _round = results.rounds[VOTES - 1]
+    except IndexError:
+        # the election was decided before the number of votes were exhausted
+        return []
     eliminated_candidates = [
         candidate_result.candidate
         for candidate_result in _round.candidate_results
         if candidate_result.status == CandidateStatus.Rejected
     ]
-    return eliminated_candidates
+    return [c.name for c in eliminated_candidates]
 
 
 def simulate_elections():
-    NUM_SIMULATIONS = 10
     results = []
     [
-        results.append(run_election(candidate_weights))
-        for n in progressbar(range(NUM_SIMULATIONS))
+        results.append(run_election(CANDIDATE_WEIGHTS))
+        for n in progressbar(range(SIMULATIONS))
     ]
     rounds = np.array([len(r.rounds) for r in results])
-    winners = np.array([r.get_winners()[0].name for r in results])
     avg_rounds = np.median(rounds)
-    winners, w_freq = np.unique(winners, return_counts=True)
 
-    eliminated = np.array([get_eliminated(r, 0)[0].name for r in results])
-    eliminated, e_freq = np.unique(eliminated, return_counts=True)
+    eliminated = []
+    [eliminated.extend(get_eliminated(r)) for r in results]
+    eliminated = np.array(eliminated)
+    eliminated, counts = np.unique(eliminated, return_counts=True)
+    eliminated_counts = zip(eliminated, counts)
+    eliminated_counts = sorted(
+        eliminated_counts, key=operator.itemgetter(1), reverse=True
+    )
 
-    print("============ REPORT ============")
-    print("---------- avg rounds ----------")
-    print(avg_rounds)
-    print("----------- winners ------------")
-    for candidate, freq in zip(winners, w_freq):
-        print(f"{candidate}: {freq}/{NUM_SIMULATIONS} wins")
-    print("------- 1st round losses -------")
-    for candidate, freq in zip(eliminated, e_freq):
-        print(f"{candidate}: {freq}/{NUM_SIMULATIONS} 1st round losses")
+    print("=================== REPORT ===================")
+    print("------------------ avg rounds ----------------")
+    print(f"candidate selected after {avg_rounds} rounds on average")
+    print("--------------- num simulations --------------")
+    print(SIMULATIONS)
+    print("------------------ num voters ----------------")
+    print(VOTERS)
+    print(f"------------- eliminated by round {VOTES} ----------")
+    for candidate, count in eliminated_counts:
+        print(f"{candidate}: {count}/{SIMULATIONS} times")
 
 
 if __name__ == "__main__":
