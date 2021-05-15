5a4c05bad1523616050b3998b0a3c0bc20ef4884
ting zhang <thetingzhang@gmail.com>
2021-05-15 17:37:51 -0400

    [Add] election simulation with pyrankvote

--- election.py
+++ election.py
@@ -1,6 +1,9 @@
 from typing import List, Tuple
 import numpy as np
 
+from pyrankvote import Candidate, Ballot, instant_runoff_voting
+from pyrankvote.helpers import ElectionResults
+
 
 candidate_weights = [
     ("andrew yang", 0.21),
@@ -14,19 +17,44 @@ candidate_weights = [
 ]
 
 
-def generate_choices(candidate_weights: List[Tuple[str, float]]):
+def generate_ballot(candidates=List[Candidate], weights=List[float]) -> Ballot:
     # rank up to 5 candidates in order of preference
     # source: https://www1.nyc.gov/site/civicengagement/voting/ranked-choice-voting.page
     votes = 5
-    candidates = [p[0] for p in candidate_weights]
+    ballot = np.random.choice(candidates, p=weights, replace=False, size=votes)
+    return Ballot(ranked_candidates=ballot)
+
+
+def run_election(candidate_weights: List[Tuple[str, float]]) -> ElectionResults:
+    # total votes in 2017 nyc democratic mayoral primary
+    # source: https://en.wikipedia.org/wiki/2017_New_York_City_mayoral_election
+    voters = 437517
+
+    candidates = [Candidate(p[0]) for p in candidate_weights]
     weights = np.array([p[1] for p in candidate_weights])
     adjusted_weights = weights / weights.sum()
-    return np.random.choice(candidates, p=adjusted_weights, replace=False, size=votes)
+
+    ballots = [generate_ballot(candidates, adjusted_weights) for v in range(voters)]
+    return instant_runoff_voting(candidates, ballots)
 
 
-def run_election():
-    # 2.97 million New York City voters cast a ballot in the 2020 general election
-    # source: https://www.gothamgazette.com/city/9907-voter-turnout-new-york-city-increased-7-5-percent-2016-to-2020
-    # voters = int(2.97 * 1000000)
-    voters = 1000
-    results = [generate_choices(candidate_weights) for v in range(voters)]
+def simulate_elections():
+    NUM_SIMULATIONS = 10
+    results = []
+    [results.append(run_election(candidate_weights)) for n in range(NUM_SIMULATIONS)]
+    rounds = np.array([len(r.rounds) for r in results])
+    winner = np.array([r.get_winners()[0].name for r in results])
+    avg_rounds = np.median(rounds)
+    winners, frequencies = np.unique(winner, return_counts=True)
+
+    print("============ REPORT ============")
+    print("---------- avg rounds ----------")
+    print(avg_rounds)
+    print("----------- winners ------------")
+    for winner, freq in zip(winners, frequencies):
+        print(f"{winner}: {freq}/{NUM_SIMULATIONS} wins")
+
+
+if __name__ == "__main__":
+    simulate_elections()
+    run_election(candidate_weights)
